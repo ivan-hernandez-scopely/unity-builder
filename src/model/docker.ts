e2f4b30d8c704709c913c9aa5cba68ead80af30d
Ivan Hernandez <ivan.hernandez@scopely.com>
2021-04-28 11:33:08 +0200

    added bash -c "ssh-add -l && ssh -T git@github.com"

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -89,6 +89,7 @@ class Docker {
         --volume /home/runner/.ssh:/root/.ssh \
         --volume /home/runner/.ssh/known_hosts:/root/.ssh/known_hosts \
         ${image} \
+        bash -c "ssh-add -l && ssh -T git@github.com"
         `;
 
     await exec(command, undefined, { silent });
